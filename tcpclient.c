cf781772e1412242627080d794ece53c97a91866
Abdoulaye <dialloablaye2003@gmail.com>
2025-11-08 22:19:17 +0100

    excercice13-socket client

---
+++ tcpclient.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+
+
+#define CLIENT "127.0.0.1"
+#define PORT 1234
+#define BUFLEN 512
+
+
+void stop(char* msg){
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char* argv[]){
+
+    int sockfd;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd == -1)
+    {
+        stop("socket()");
+    }
+    //printf("socket créé\n");
+
+    
+
+
+
+
+
+
+}
